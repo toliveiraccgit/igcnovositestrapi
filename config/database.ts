99783773825d049f5d5053b40f98618901e44179
Caue-Mendes <mendes.caue@outlook.com>
2023-08-05 17:02:15 -0300

    Date adjust and cookies

--- config/database.ts
+++ config/database.ts
@@ -1,29 +1,29 @@
 // Local Teste
-export default ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: "3.136.141.152",
-      port: 5432,
-      database: "igc_database",
-      user: "admin",
-      password: "Sage@2854",
-      timezone: env("TIMEZONE", "America/Sao_Paulo"),
-    },
-  },
-});
-
-// Original
 // export default ({ env }) => ({
 //   connection: {
 //     client: "postgres",
 //     connection: {
-//       host: "localhost",
+//       host: "3.136.141.152",
 //       port: 5432,
-//       database: "igcp_database",
-//       user: "postgres",
-//       password: "postgres",
+//       database: "igc_database",
+//       user: "admin",
+//       password: "Sage@2854",
 //       timezone: env("TIMEZONE", "America/Sao_Paulo"),
 //     },
 //   },
 // });
+
+// Original
+export default ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: "localhost",
+      port: 5432,
+      database: "igcp_database",
+      user: "postgres",
+      password: "postgres",
+      timezone: env("TIMEZONE", "America/Sao_Paulo"),
+    },
+  },
+});
